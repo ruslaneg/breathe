ec8be50750a8d840d91156d2b1379465f76a4ba7
Ruslan Egorov <84963382+ruslanegorov@users.noreply.github.com>
2025-12-03 13:01:09 +0300

    Fix prettier in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'eslint/config';
 import eslintJs from '@eslint/js';
 import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   eslintJs.configs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
